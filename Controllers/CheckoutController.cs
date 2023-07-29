7c0f8af3663c53ae1fbac0052454b540f370b96e
Deepak <deep24kumar24@gmail.com>
2023-07-29 02:48:59 -0400

    Fixing some code and added dropIn option as well

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -7,9 +7,15 @@ namespace CakeCapitalCheckout.Controllers
     public class CheckoutController : Controller
     {
         [Route("/")]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(bool dropIn = false)
         {
-            return View();
+            if (dropIn == false)
+                return View();
+            else
+            {
+                var response = await CreateIntent("USD");
+                return View("IndexDropIn", response);
+            }
         }
 
         [HttpGet("payment-view")]
@@ -17,16 +23,7 @@ namespace CakeCapitalCheckout.Controllers
         {
             try
             {
-                var token = await GetToken();
-
-                AirwallexCreateIntentRequestContract requestContract = new(500.0m, countryCode.ToUpper(), Guid.NewGuid().ToString(), Guid.NewGuid(), "https://localhost:7103/");
-
-                var client = new RestClient("https://api-demo.airwallex.com/api/v1/pa/payment_intents/create");
-                var request = new RestRequest("", Method.Post);
-                request.AddHeader("Content-Type", "application/json");
-                request.AddHeader("Authorization", $"Bearer {token.Token}");
-                request.AddJsonBody(requestContract);
-                var response = await client.PostAsync<AirwallexPaymentIntent>(request);
+                var response = await CreateIntent(countryCode);
                 return PartialView("_PaymentView", response);
             }
             catch(Exception ex)
@@ -43,6 +40,20 @@ namespace CakeCapitalCheckout.Controllers
             request.AddHeader("x-api-key", "17fbc7b14e99dc74a3f1070c83b06a3c922cc687bbbf1f6ed77033cf6b3b333b0fe56812517201dbafd3af108a3188f8");
             request.AddHeader("x-client-id", "fTJn6LvCQgGFham22cOmzA");
             return await client.PostAsync<AirwallexToken>(request);
+        }
+
+        private static async Task<AirwallexPaymentIntent> CreateIntent(string countryCode)
+        {
+            var token = await GetToken();
+
+            AirwallexCreateIntentRequestContract requestContract = new(500.0m, countryCode.ToUpper(), Guid.NewGuid().ToString(), Guid.NewGuid(), "https://localhost:7103/");
+
+            var client = new RestClient("https://api-demo.airwallex.com/api/v1/pa/payment_intents/create");
+            var request = new RestRequest("", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", $"Bearer {token.Token}");
+            request.AddJsonBody(requestContract);
+            return await client.PostAsync<AirwallexPaymentIntent>(request);
         }
     }
 }
