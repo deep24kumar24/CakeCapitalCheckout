33b512005d933e05ffd397bfe74ab44bc1206ad0
Deepak <deep24kumar24@gmail.com>
2023-08-07 00:23:36 -0400

    Some fixes

---
+++ Controllers/PrivacyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CakeCapitalCheckout.Controllers
+{
+    public class PrivacyController: Controller
+    {
+        [Route("privacy")]
+        [Route("privacy-policy")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
